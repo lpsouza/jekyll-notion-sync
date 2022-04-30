a4d2cfa2e470e92e6abd6a9220bafa6b62c7c124
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-04-30 01:43:42 -0300

    Improve CLI and changes to consitence on add notion pages

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { Client } from "@notionhq/client";
-import { Octokit } from "@octokit/rest";
+import { Client } from '@notionhq/client';
+import { Octokit } from '@octokit/rest';
 import fm from 'front-matter';
+import table from 'cli-table';
+import color from 'cli-color';
 
 const notion = new Client({
     auth: process.env.NOTION_TOKEN
@@ -14,50 +16,75 @@ const octokit = new Octokit({
 });
 
 (async () => {
-    const database = await notion.search({ filter: { property: "object", value: "database" } });
+    const database = await notion.search({ filter: { property: 'object', value: 'database' } });
     const databaseId = database.results[0].id;
     const databasePosts = await notion.databases.query({ database_id: databaseId });
     const postsFromNotion = databasePosts.results.map(post => post);
 
     const owner = process.env.GITHUB_OWNER;
     const repo = process.env.GITHUB_REPO;
-    const gitFiles = await octokit.rest.repos.getContent({ owner, repo, path: '_posts' });
+    const posts = (await octokit.rest.repos.getContent({ owner, repo, path: '_posts' })).data as any[];
 
-    const posts = gitFiles.data as any[];
+    let countOk = 0;
+    let countNo = 0;
+    let countSkip = 0;
+    const filesNo = [];
 
-    posts.forEach(async post => {
-        const contentEncoded = await (await octokit.rest.repos.getContent({ owner, repo, path: `_posts/${post.name}` })).data["content"];
+    for (const post of posts) {
+        const contentEncoded = await (await octokit.rest.repos.getContent({ owner, repo, path: `_posts/${post.name}` })).data['content'];
         const contentRaw = Buffer.from(contentEncoded, 'base64').toString('utf-8');
         const content = fm(contentRaw);
 
-        if (!postsFromNotion.find(post => post["properties"]["Title"]["title"][0]["plain_text"] == content.attributes["title"])) {
+        if (!postsFromNotion.find(a => a['properties']['Title']['title'][0]['plain_text'] === content.attributes['title'])) {
             const parent = { database_id: databaseId };
-            const cover = content.attributes["image"] != null ? { external: { url: content.attributes["image"] } } : null;
+            const cover = content.attributes['image'] != null ? { external: { url: content.attributes['image'] } } : null;
             const children = [];
             const properties = {};
-            properties["Author"] = { people: [{ id: process.env.NOTION_USER }] };
-            properties["Created"] = { date: { start: new Date(content.attributes["date"]).toISOString() } };
-            properties["Category"] = { select: { name: content.attributes["category"] } };
-            properties["Published"] = { checkbox: content.attributes["published"] };
-            properties["Title"] = { title: [{ text: { content: content.attributes["title"] } }] };
-            properties["Tags"] = { multi_select: [] };
+            properties['Author'] = { people: [{ id: process.env.NOTION_USER }] };
+            properties['Created'] = { date: { start: new Date(content.attributes['date']).toISOString() } };
+            properties['Category'] = { select: { name: content.attributes['category'] } };
+            properties['Published'] = { checkbox: content.attributes['published'] };
+            properties['Title'] = { title: [{ text: { content: content.attributes['title'] } }] };
+            properties['Tags'] = { multi_select: [] };
 
-            if (content.attributes["tags"]) {
-                content.attributes["tags"].forEach(tag => {
-                    properties["Tags"]["multi_select"].push({ name: tag });
+            if (content.attributes['tags']) {
+                content.attributes['tags'].forEach(tag => {
+                    properties['Tags']['multi_select'].push({ name: tag });
                 });
             }
 
-            content.body.split("\n").forEach(line => {
-                if (line != "") {
-                    children.push({ paragraph: { rich_text: [{ text: { content: line } }] } });
+            content.body.split('\n').forEach(line => {
+                if (line != '') {
+                    if (line.startsWith('###')) {
+                        children.push({ heading_3: { rich_text: [{ text: { content: line.replace('###', '') } }] } });
+                    } else {
+                        children.push({ paragraph: { rich_text: [{ text: { content: line } }] } });
+                    }
                 }
             });
 
-            notion.pages.create({ parent, cover, properties, children }).catch(err => {
-                console.log(`${post.name} failed to create`);
-            });
-        }
-    });
+            try {
+                const page = await notion.pages.create({ parent, cover, properties, children });
+                countOk++;
+            } catch (error) {
+                countNo++;
+                filesNo.push(post.name);
+            }
 
+        } else {
+            countSkip++;
+        }
+    };
+
+    const status = new table();
+    status.push(['Processed posts', color.green(countOk)]);
+    status.push(['Posts with errors', color.red(countNo)]);
+    status.push(['Skipped posts', color.yellow(countSkip)]);
+    console.log(status.toString());
+
+    if (filesNo.length > 0) {
+        const files = new table({ head: [color.red('File on error')] });
+        filesNo.forEach(file => files.push([file]));
+        console.log(files.toString());
+    }
 })();
